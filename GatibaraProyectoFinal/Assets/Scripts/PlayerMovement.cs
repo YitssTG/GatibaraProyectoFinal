7403a6d13ac5144855f73bbb5155843440a2cb49
Firestar04O <ricardo.espino.b@tecsup.edu.pe>
2025-05-22 11:10:33 -0500

    movement implementated

--- GatibaraProyectoFinal/Assets/Scripts/PlayerMovement.cs
+++ GatibaraProyectoFinal/Assets/Scripts/PlayerMovement.cs
@@ -1,24 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
+using DG.Tweening.Core.Easing;
 using UnityEngine;
-
-public class PlayerMovement : MonoBehaviour
+using UnityEngine.InputSystem;
+public class CharacterController3D : MonoBehaviour
 {
-    public Player player;
-    Rigidbody myrgbd;
-    float horizontal;
-    float transversal;
+    [Header("Player Movement Properties")]
+    [SerializeField] private float speed;
+    [SerializeField] Vector2 movementInput;
+
+    [Header("Raycast Properties")]
+    [SerializeField] private Transform _origin;
+    [SerializeField] private Vector3 _direction = Vector3.down;
+    [SerializeField] private float _distance;
+    [SerializeField] private LayerMask _layerInteraction;
+
+    [Header("DrawRay Properties")]
+    [SerializeField] private Color OnCollisionRay = Color.green;
+    [SerializeField] private Color OnNotCollisionRay = Color.white;
+
+    private Rigidbody _rigidbody;
+    private bool isMoving;
+
     private void Awake()
     {
-        myrgbd = GetComponent<Rigidbody>();
+        _rigidbody = GetComponent<Rigidbody>();
     }
     private void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-        transversal = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(movementInput.x, 0f, movementInput.y);
+        transform.Translate(direction * speed * Time.deltaTime);
+
+        isMoving = direction.magnitude > 0.1f;
+        RaycastHit hit;
+        if (Physics.Raycast(_origin.position, _direction, out hit, _distance, _layerInteraction))
+        {
+            Debug.DrawRay(_origin.position, _direction * _distance, OnCollisionRay);
+        }
+        else
+        {
+            Debug.DrawRay(_origin.position, _direction * _distance, OnNotCollisionRay);
+        }
     }
-    private void FixedUpdate()
+    public void OnMove(InputAction.CallbackContext context)
     {
-        myrgbd.linearVelocity = new Vector3(horizontal * player.speed, 0, transversal * player.speed);
+        movementInput = context.ReadValue<Vector2>();
     }
 }
