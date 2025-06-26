0eebd503ee5aa4045c9a3b26cd04383a5145e2e7
Firestar04O <ricardo.espino.b@tecsup.edu.pe>
2025-06-25 21:23:38 -0500

    CooldownMechanic base

---
+++ GatibaraProyectoFinal/Assets/Scripts/CooldownMechannic/AbilityCooldownData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class AbilityCooldownData
+{
+    public CombinationData ability;
+    public float cooldownDuration;
+    public float cooldownRemaining;
+    public bool isOnCooldown;
+}
